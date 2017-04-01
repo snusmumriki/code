6f473c1c927b839a8e24cc3a0b219878daed2fa6
megaman <wdart1995@gmail.com>
2017-04-02 00:09:46 +0200

    1

--- prog1.py
+++ prog1.py
@@ -1,1 +1,30 @@
-print(1)
+class Monster:
+    def __init__(self, time, health):
+        self.time = time
+        self.health = health
+
+
+class Word:
+    def __init__(self, damage, rate):
+        self.damage = damage
+        self.rate = rate
+
+
+def func():
+    while mana:
+        pass
+    j += True
+
+
+n, mana = map(int, input().split())
+words = []
+monsters = []
+j = 0
+for _ in range(n):
+    k, a, b = map(int, input().split())
+    a = (a + j) % 1000001
+    b = (b + j) % 1000001
+    if k == 1:
+        words.append(Word(a, b))
+    else:
+        monsters.append(Monster(a, b))
