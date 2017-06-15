04b95c741003319582d37cf6b22312946e97367c
megaman <wdart1995@gmail.com>
2017-06-15 23:26:43 +0200

    1

--- prog2.py
+++ prog2.py
@@ -1,13 +1,16 @@
-n = int(input())
-coms = [tuple(map(int, input().split())) for _ in range(0, n)]
-nums = set()
-for com in coms:
-    tmp = set(range(com[1], com[2] + 1))
-    if com[0] == 1:
-        nums |= tmp
-    elif com[0] == 2:
-        nums -= tmp
-    else:
-        nums ^= tmp
-    tmp = set(range(1, max(tmp) + 2)) - nums
-    print(min(tmp))
+try:
+    n = int(input())
+    coms = [tuple(map(int, input().split())) for _ in range(0, n)]
+    nums = set()
+    for com in coms:
+        tmp = set(range(com[1], com[2] + 1))
+        if com[0] == 1:
+            nums |= tmp
+        elif com[0] == 2:
+            nums -= tmp
+        else:
+            nums ^= tmp
+        tmp = set(range(1, max(tmp) + 2)) - nums
+        print(min(tmp) if tmp else 1)
+except Exception as e:
+    print(e)
